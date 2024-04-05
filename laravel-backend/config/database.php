8327a8a6c96241fc0962c0208d36a3ad9e6f55d4
medhatelmasry <medhatelmasry@live.com>
2024-04-04 22:06:58 -0700

    new entries in yml11

--- laravel-backend/config/database.php
+++ laravel-backend/config/database.php
@@ -34,8 +34,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            // 'database' => env('DB_DATABASE', database_path('school.sqlite')),
-            'database' => database_path(env('DB_DATABASE')),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
